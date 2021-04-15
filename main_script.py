c83c2f6651a60a53b5093ea2f35721e6c9b1ebcc
Carolina Valani <carolinavalanic@gmail.com>
2021-04-14 22:08:13 -0300

    Update main_script.py

--- main_script.py
+++ main_script.py
@@ -14,8 +14,18 @@ text_path = input("What is the path for your text? Please, notice that your file
 read_text = open(text_path)
 data_text = read_text.readlines()
 
-# Treat the text so it can be ready to translate
-text_splitted = re.split('(\W+)',data_text[0])
+# Remove the line breaks (\n)
+data_text_adjusted = []
+for item in data_text:
+    text_item = item.replace("\n","")
+    data_text_adjusted.append(text_item) 
+
+# Create function to split text
+data_text_splitted = data_text_adjusted
+count = 0
+for item in data_text_adjusted:
+    data_text_splitted[count] = re.split('(\W+)',item)
+    count = count + 1
 
 # Create function to test if the word exists in the dictionary and save it in a object
 def test_word_in_dict(word, dictionary):
@@ -50,17 +60,29 @@ def translator(text):
     Outputs: 
     There are no outputs.
     """
-    count = 0 
-    text_copy = text
-    for word in text:
-        text_copy[count] = test_word_in_dict(word,lol_dict)
-        count = count + 1
-    return ''.join(text_copy)
+     
+    text_copy = []
+    for text_list in text:
+        count = 0
+        for word in text_list:
+            text_list[count] = ((test_word_in_dict(word.casefold(),lol_dict)[0].upper() + test_word_in_dict(word.casefold(),lol_dict)[1:]) if len(word) > 1 and word[0].isupper() else test_word_in_dict(word.casefold(),lol_dict))
+            count = count + 1
+        agg = ''.join(text_list)
+        text_copy.append(agg)
+    return text_copy
 
-# Save the result in the 
-save = open(text_path.split('.')[0]+'_lolcat.'+text_path.split('.')[1],'w').write(translator(text_splitted)) 
+#Apply function to the treated text
+final_text = translator(data_text_splitted)
 
-if save > 0:
+# Save the output in a new file
+save_file = open(text_path.split('.')[0]+'_lolcat.'+text_path.split('.')[1],'w')
+for lines in final_text:
+    row = lines+'\n'
+    save_file.write(row)
+save_file.close()  
+
+
+if len(open(text_path.split('.')[0]+'_lolcat.'+text_path.split('.')[1],'r').readlines()) > 0:
     print("That's all, check your folder and find the file "+ text_path.split('.')[0] +'_lolcat.'+text_path.split('.')[1]+" :)")
 else:
     print("Oh no, something went wrong!")
